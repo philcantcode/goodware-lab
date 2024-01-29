b4336b15a5523d9c4f63098fc84e820931f0ca35
philcantcode <20229771+philcantcode@users.noreply.github.com>
2024-01-29 17:19:59 +0000

    updated pe parser to have args

--- file_parsing/pe_parser/main.go
+++ file_parsing/pe_parser/main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/philcantcode/mal-lab/file_parsing/pe_parser/pipeline"
 )
 
-const DLL_PATH = "../../payloads/calc_exe/calc.dll"
-
 func main() {
-	pe, err := pipeline.ParsePEx64(DLL_PATH)
+	peFile := flag.String("pe-file", "../../payloads/calc_exe/calc.dll", "Path to the PE file")
+	display := flag.String("display", "all", "Field to display: imports, exports, all")
+
+	flag.Parse()
+
+	pe, err := pipeline.ParsePEx64(*peFile)
 	if err != nil {
 		log.Fatalf("Error parsing PE: %v", err)
 	}
 
-	fmt.Printf("Imports: %+v\n", pe.ImportFuncNames)
+	switch *display {
+	case "imports":
+		displayNumberedArray(pe.ImportFuncNames)
+	case "exports":
+		displayNumberedArray(pe.ExportFuncNames)
+	case "all":
+		fmt.Printf("%+v\n", pe.ImportFuncNames)
+	default:
+		fmt.Println("Invalid display field")
+	}
+}
+
+func displayNumberedArray(arr []string) {
+	for i, s := range arr {
+		fmt.Printf("%d. %s\n", i+1, s)
+	}
 }
